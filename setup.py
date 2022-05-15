3de5609235d173d29ad9bd9ed7bdfe2a813ab1bd
Vipul Gharde <vipul.gharde@gmail.com>
2022-05-14 22:41:17 -0400

    Bump version: 0.4.0 → 0.5.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='slr-parser',
-    version='0.4.0',
+    version='0.5.0',
     author='Vipul Gharde',
     authon_email='vipul.gharde@gmail.com',
     description='Implementation of Simple LR (SLR) Parser for educational purposes.',
